4345e8a5f73363f09dd142b8ad38380120dc99be
mhw32 <me@mikewuis.me>
2022-09-29 12:34:05 +0800

    setup as a package

--- pareto/__init__.py
+++ pareto/__init__.py
@@ -1,1 +1,11 @@
-from client import *
+from pareto.client import Client
+from pareto.constants import (ORDER_SIDE_BUY,
+                              ORDER_SIDE_SELL,
+                              ORDER_TYPE_CALL,
+                              ORDER_TYPE_PUT,
+                              UNDERLYING_ETH,
+                              VALID_STRIKE,
+                              VALID_ORDER_SIDE,
+                              VALID_ORDER_TYPE,
+                              VALID_UNDERLYING,
+                              )
