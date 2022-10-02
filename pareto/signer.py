f2270eb866585b6af9458f7608652b8c757633d2
mhw32 <me@mikewuis.me>
2022-10-02 16:54:03 +0800

    Updates to signer

--- pareto/signer.py
+++ pareto/signer.py
@@ -1,3 +1,4 @@
+import time
 from eth_account import Account
 from eth_account.messages import encode_defunct
 
@@ -12,15 +13,36 @@ class Signer:
         self.address = Account.from_key(private_key).address
         self._private_key = private_key
 
-    def sign(self, message):
+    def sign(self,
+             method,
+             uri,
+             body,
+             timestamp,
+             ):
+        r"""Sign a structured message for the backend to parse.
+        Arguments:
+        --
+        method (string): GET or POST
+        uri (string): Endpoint path e.g. /ping
+        body (Object): Body of the request
+        timestamp (integer): Timestamp of the request
+        """
         # https://eth-account.readthedocs.io/en/stable/eth_account.html#eth_account.account.Account.sign_message
-        message_hash = encode_defunct(text=message)
+        message = {
+            'method': method,
+            'requestPath': uri,
+            'body': body,
+            'timestamp': timestamp,
+        }
+        message_hash = encode_defunct(message)
         signed_message = Account.sign_message(message_hash, 
                                               self._private_key)
         return signed_message.signature
 
     def add_headers(self, message, header={}):
+        timestamp = int(time.time())
         signature = self.sign(message)
-        header['address'] = self.address
-        header['signature'] = signature
+        header['pareto-ethereum-address'] = self.address
+        header['pareto-signature'] = signature
+        header['pareto-timestamp'] = timestamp
         return header
